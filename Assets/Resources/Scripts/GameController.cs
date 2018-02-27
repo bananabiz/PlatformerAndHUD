7748336b9501a19e849b13737e0e47914a376f0b
bananabiz <lee21acc973@163.com>
2018-02-27 11:52:24 +1100

    2D movement, timer/health UI

---
+++ Assets/Resources/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public float currentTime;
+    public Text timeDisplay;
+    public int health;
+    public Text healthDisplay;
+
+	
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currentTime += Time.deltaTime;
+
+        timeDisplay.text = currentTime.ToString("0:00");
+
+        healthDisplay.text = health.ToString();
+	}
+
+    void OnCollisionEnter2D(Collision2D obj)
+    {
+        if (obj.gameObject.CompareTag("hazard"))
+        {
+            health--;
+        }
+
+    }
+
+}
